46ea79295be8c15dddeee9eea5eb1d93b5bddf4d
Chris Conover <crconover@gmail.com>
2015-03-03 20:55:39 -0800

    Change sign to negative before  the 5.
    The book described the polynomial as 3x^3 - 5x^2 + 6.

--- Chapter 04/Exercise 4.04/Exercise 4.04/main.m
+++ Chapter 04/Exercise 4.04/Exercise 4.04/main.m
@@ -14,7 +14,7 @@ int main(int argc, const char * argv[])
     
     @autoreleasepool {
         double x = 2.55;
-        double polynomial = 3 * pow(x, 3) + 5 * pow(x, 2) + 6;
+        double polynomial = 3 * pow(x, 3) - 5 * pow(x, 2) + 6;
         NSLog(@"Value of polynomial %f", polynomial);
     }
     return 0;
